fdf231a24f65386d099ca4d3bd6aaf5f12831146
Hélder Bacanhim <helderperestrelo99@gmail.com>
2019-03-27 21:35:17 +0000

    vscode working on c++

--- ex2.cpp
+++ ex2.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-int x=10;
+int x=200;
 main(){
     x+=1;
     cout<<x<<"\n";
--- ex8.cpp
+++ ex8.cpp
@@ -5,9 +5,10 @@ main(){
 	int v1 = 3;
 	int v2 = 2;
 	if (v1>v2){
-		cout<< "o "<<v1<< " e maior que "<<v2;
+		cout<< "o "<<v1<< " e maior que "<<v2<<"\n";
 	}
 	else if (v2>v1){
-		cout<< "o "<<v2<< " e maior que "<<v1;
+		cout<< "o "<<v2<< " e maior que "<<v1<<"\n";
 	}
+	system("PAUSE");
 }
